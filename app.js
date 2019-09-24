c3fd8464bdf11b709ac3628f1a45f98bd61d4984
AarnoT <AarnoT@users.noreply.github.com>
2019-09-24 21:18:29 +0300

    Add start screen

--- app.js
+++ app.js
@@ -3,15 +3,39 @@ function main() {
     const ctx = canvas.getContext("2d");
     const video = document.querySelector("#drawvideo");
 
+    function drawStartScreen() {    
+        video.pause();
+
+        ctx.beginPath();
+        ctx.rect(0, 0, 500, 500);
+        ctx.fillStyle = "lightgrey";
+        ctx.fill();
+
+        ctx.fillStyle = "white";
+        ctx.font = "30px Arial";
+        const txt = "Click to start";
+        const size = ctx.measureText(txt);
+        ctx.fillText(txt, canvas.width/2 - size.width/2, canvas.height/2);
+    }
+
     video.addEventListener("play", () => {
         function step() {
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            requestAnimationFrame(step);
+            if (!video.paused) {
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                requestAnimationFrame(step);
+            }
         }
         requestAnimationFrame(step);
+        setTimeout(drawStartScreen, 15000);
     });
 
-    video.play();
+    canvas.addEventListener("click", () => {
+        if (video.paused) {
+            video.play();
+        }
+    });
+
+    drawStartScreen();
 }
 
 window.onload = main;
