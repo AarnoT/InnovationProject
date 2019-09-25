770fb7f59c1172ba0acf3b2c2b6624286642e145
Jingxuaw <jingxuaw@metropolia.fi>
2019-09-25 23:54:59 +0800

    add database
    add database both for register and login pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -11,11 +12,72 @@ def main(path='index'):
         return render_template('index.html')
     elif path == 'app':
         return render_template('app.html')
+     elif path == 'register':
+        return render_template('register.html')
     elif path == 'app.js':
         with open('app.js') as f:
             return f.read()
     else:
         abort(404)
+
+        
+@app.route('/registuser')
+def getRigistRequest():
+    user_id = request.args.get('user')
+    user_pwd = request.args.get('password')
+
+    conn = sqlite3.connect('app.db')
+
+    c = conn.cursor()
+    c.execute('''CREATE TABLE info(name text, password text)''')
+   
+
+    try:
+    
+        c.execute("INSERT INTO app VALUES (user_id,user_pwd)")
+
+        conn.commit()
+        return render_template('index.html')
+    except:
+
+        traceback.print_exc()
+       
+        c.rollback()
+        return 'Register failed'
+
+    conn.close()
+
+@app.route('/login')
+def getLoginRequest():
+    
+    conn = sqlite3.connect('app.db')
+    c = conn.cursor()
+    user_id = request.args.get('name')
+    user_pwd = request.args.get('password')
+    
+    try:
+        c.execute('SELECT * FROM stocks WHERE symbol=?, ?', user_id, user_pwd)
+       
+        results = cursor.fetchall()
+        print(len(results))
+        if len(results) == 1:
+            data = {}
+            data["code"] = 'Welcome'
+            for row in results:
+                data["id"] = row[0]
+                data["pwd"] = row[1]
+            return render_template('app.html')
+        else:
+            return 'Username or password incorrect!'
+
+        conn.commit()
+    except:
+
+        traceback.print_exc()
+        c.rollback()
+
+    conn.close()
+
 
 
 if __name__ == '__main__':
