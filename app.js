a540cc0fe0e56b45b1935132827d6e44394760bb
Jingxuaw <jingxuaw@metropolia.fi>
2019-10-04 16:09:47 +0800

    modify the text position

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ function drawStatusScreen() {
         const size4 = ctx.measureText(txt4);
 	const size5 = ctx.measureText(txt5);
         ctx.fillText(txt3, canvas.width/2 - size3.width/2, canvas.height/5 * 4);
-        ctx.fillText(txt4, canvas.width/2 - size4.width/2, canvas.height/5 * 9);
-	ctx.fillText(txt5, canvas.width/2 - size5.width/2, canvas.height/5 * 13);
+        ctx.fillText(txt4, canvas.width/2 - size4.width/2, canvas.height/10 * 9);
+	ctx.fillText(txt5, canvas.width/2 - size5.width/2, canvas.height/15 * 18);
     }
 
     audio.play();
